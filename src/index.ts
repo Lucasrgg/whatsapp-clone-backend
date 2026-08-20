e68e5dfc00b148696a1ae99330e8e7b78f791801
Lucas <lukrodrigues307@gmail.com>
2026-08-19 21:20:46 -0300

    Ajustes no servidor Express

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-console.log("Servidor iniciando...");
+import express from "express";
+
+const app = express();
+const PORT = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Servidor rodando!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
+});
